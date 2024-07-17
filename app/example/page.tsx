4920f06cbb3f82684a65c199f571d0335faf4913
baurine <2008.hbl@gmail.com>
2024-07-17 16:48:55 +0800

    feat: example page

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,7 +1,12 @@
+import dynamic from "next/dynamic";
+
+// editor-example must have default export when it's imported dynamically
+const EditorExample = dynamic(() => import("./editor-example"), { ssr: false });
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+    <main className="h-screen">
+      <EditorExample />
     </main>
   );
 }
